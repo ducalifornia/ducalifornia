4db12ae543f9c0872c5b26ee2b8590ba716f18b6
Ivan Rodriguez <ivan.rodriguez001@gmail.com>
2013-11-12 18:57:06 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ducalifornia::Application.config.secret_key_base = 'd64b096db36e37be42a31100a4824d08364f6292835990e4e150e771ea795c37e2d86e122d529587e10adcba194f244354c33457b8e3e8332f60751c1e423e31'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ducalifornia::Application.config.session_store :cookie_store, key: '_ducalifornia_session'
